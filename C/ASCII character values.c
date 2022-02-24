20e950eaf0aa82fb67aff398993895d9778e61c8
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-02-24 19:08:14 +0530

    ASCII & values

---
+++ C/ASCII character values.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+//ASCII character and their values
+int main(){
+        int i;
+        for(i = 0; i<=255; i++)
+                printf("The ASCII value of character %c is %d\n",i,i);
+        return 0;
+
+}
